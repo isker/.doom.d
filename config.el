fefb8d094e021997640d8da3c54020c3376cd24c
Ian Kerins <ianskerins@gmail.com>
2022-03-03 20:25:46 -0500

    Fix evil-markdown-mode-map keybindings

--- config.el
+++ config.el
@@ -82,10 +82,11 @@
  :mode vterm-copy-mode
  :n "i" 'vterm-copy-mode)
 
-(map!
- :map evil-markdown-mode-map
- ;; Too addicted to emacs navigation in insert mode; go back a word instead of bolding.
- :i "M-b" nil)
+(after! evil-markdown
+  (map!
+   :map evil-markdown-mode-map
+   ;; Too addicted to emacs navigation in insert mode; go back a word instead of bolding.
+   :i "M-b" nil))
 
 (after! counsel-projectile
   (counsel-projectile-modify-action
